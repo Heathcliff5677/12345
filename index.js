01c52d8a547aa09d5231bad7d702f259e7762bb0
Heathcliff5677 <103395672+Heathcliff5677@users.noreply.github.com>
2022-04-19 11:29:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction(){
+     
+     
+     let x = document.getElementById("userInput1").value;
+     let y = document.getElementById("userInput2").value;
+     let z =parseInt(x)+parseInt(y);
+     document.getElementById("demo").innerHTML = z;
+    }
+    //Heathcliff Wang Li
+    //1. Change the page structure 2. Dynamically generate nodes 3. String processing
